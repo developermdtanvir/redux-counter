2d1b392c84ea33c5274a4ccc4c3287922c42fa16
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-07-22 23:00:12 +0600

    add: script.js file && select dom element

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// select dom element with querySelector
+const state = document.querySelector('#state');
+const incriment = document.querySelector('#incriment');
+const decriment = document.querySelector('#decriment');
+
+incriment.addEventListener('click', () =>{
+    console.log('incriment clicked');
+})
