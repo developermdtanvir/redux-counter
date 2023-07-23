61415e22df096f77174147195d23000a0f42490f
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-07-23 14:27:34 +0600

    add: action cratiors and identifiars

--- script/script.js
+++ script/script.js
@@ -3,6 +3,24 @@ const incrimentEl = document.querySelector("#incriment");
 const decrimentEl = document.querySelector("#decriment");
 const counterEl = document.querySelector("#counter");
 
+// action identifer
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+// action creation function
+const increment = (value) => {
+  return {
+    type: INCREMENT,
+    value: value || 5,
+  };
+};
+const decrement = (value) => {
+  return {
+    type: DECREMENT,
+    value: value || 2,
+  };
+};
+
 // initial state
 const initialState = {
   value: 0,
@@ -13,18 +31,18 @@ console.log(initialState);
 
 const counterReducer = (state = initialState, action) => {
   console.log(action);
-  if (action.type === "INCRIMENT") {
+  if (action.type === INCREMENT) {
     return {
       ...state,
-      value: state.value + 1,
+      value: state.value + action.value,
     };
-  } else if (action.type === "DECRIMENT") {
+  } else if (action.type === DECREMENT) {
     if (state.value === 0) {
       return;
     }
     return {
       ...state,
-      value: state.value - 1,
+      value: state.value - action.value,
     };
   } else {
     return state;
@@ -47,12 +65,8 @@ store.subscribe(render);
 // event handlers
 
 incrimentEl.addEventListener("click", () => {
-  store.dispatch({
-    type: "INCRIMENT",
-  });
+  store.dispatch(increment(10));
 });
 decrimentEl.addEventListener("click", () => {
-  store.dispatch({
-    type: "DECRIMENT",
-  });
+  store.dispatch(decrement(5));
 });
